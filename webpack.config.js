74e6b0c020bbf1bf93acfc9540d7bacee19f5885
ocReaper <ocreaper@gmail.com>
2017-03-03 09:00:12 +0100

    add json-loader to use sindrehorus/got without hack

--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,47 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  entry: {
-    index: './src/index'
-  },
-  output: {
-    path: './dist',
-    libraryTarget: 'commonjs2',
-    filename: 'index.js'
-  },
-  resolve: {
-    extensions: ['.js'],
-    modules: [
-      path.resolve('./src'),
-      path.resolve('./node_modules'),
-    ]
-  },
-  target: 'electron-renderer',
-  module: {
-    rules: [{
-      test: /\.jsx?$/,
-      use: {
-        loader: 'babel-loader'
-      },
-      exclude: /node_modules/
-    }, {
-      test: /\.css$/,
-      use: [{
-        loader: 'style-loader'
-      }, {
-        loader: 'css-loader',
-        query: {
-          modules: true
+    entry: {
+        index: './src/index'
+    },
+    output: {
+        path: './dist',
+        libraryTarget: 'commonjs2',
+        filename: 'index.js'
+    },
+    resolve: {
+        extensions: ['.js', '.json'],
+        modules: [
+            path.resolve('./src'),
+            path.resolve('./node_modules'),
+        ]
+    },
+    target: 'electron-renderer',
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015']
+            }
+        }, {
+            test: /\.css$/,
+            use: [{
+                loader: 'style-loader'
+            }, {
+                loader: 'css-loader',
+                query: {
+                    modules: true
+                }
+            }]
+        }, {
+            test: /\.png$/,
+            loader: 'url-loader'
+        }, {
+            test: [/package$/, /package.json$/],
+            use: 'json-loader'
         }
-      }]
-    }, {
-      test: /\.png$/,
-      loader: 'url-loader'
+        ]
     }
-    ]
-  }
 };
